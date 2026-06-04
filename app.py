e21033f56152d8c0fa7d4fd82a0fbd4b976b6743
Ariledla <arielsaradilla7@gmail.com>
2026-06-04 19:41:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 # ============================================================
 
 st.set_page_config(
-    page_title="Simulasi Sampah Kota Bandung",
+    page_title="Simulassi Sampah Kota Bandung",
     page_icon="♻️",
     layout="wide",
     initial_sidebar_state="auto"
