bafd050db5e089142f10435f2462b115802f8021
Ariledla <arielsaradilla7@gmail.com>
2026-06-05 15:56:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2507,7 +2507,7 @@ st.markdown(
        Kalau masih terlalu turun, ubah -345px jadi -365px.
        Kalau terlalu naik/kepotong, ubah jadi -320px. */
     div[data-testid="stElementContainer"]:has(.hero) {
-        margin-top: -190px !important;
+        margin-top: -210px !important;
         margin-bottom: 18px !important;
         padding-top: 0px !important;
         padding-bottom: 0px !important;
